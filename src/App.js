48d70de7298aa5a0ddc7624e7994ba745542d545
ec22761 <ec22761@qmul.ac.uk>
2024-03-13 12:53:40 +0000

    Refactor code formatting and styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './css/App.css';
 import Header from './Header';
 import Dashboard from './Dashboard';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
 
 const App = () => {
 
-    return (
-      <div>
-        <Header />
-        <Dashboard />
-        <Footer />
-      </div>
+  return (
+    <div>
+      <Header />
+      <Dashboard />
+      <Footer />
+    </div>
   );
 };
 export default App;
--- src/Footer.js
+++ src/Footer.js
@@ -1,34 +1,34 @@
 import './css/App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Footer = () => {
 
     return (
-      <footer>
-        <div className='footer-content'>
-            <div className='footer-content-item'>
-                <h2 className='text-footer-title'>Copyright</h2>
-                <p className='text-footer'>This is an academic project. Please do not distribute or contribute.</p>
+        <footer>
+            <div className='footer-content'>
+                <div className='footer-content-item'>
+                    <h2 className='text-footer-title'>Copyright</h2>
+                    <p className='text-footer'>This is an academic project. Please do not distribute or contribute.</p>
+                </div>
+                <div className='v-div'></div>
+                <div className='footer-content-item'>
+                    <h2 className='text-footer-title'>Links</h2>
+                    <article>
+                        <p className='text-footer'>Dashboard<br />Issues<br />EC</p>
+                        <p className='text-footer'>Services<br />FAQ<br />Account</p>
+                    </article>
+                </div>
+                <div className='v-div'></div>
+                <div className='footer-content-item'>
+                    <h2 className='text-footer-title'>Authors</h2>
+                    <article>
+                        <p className='text-footer'>Roman Pretty<br />Aarti Mukesh Chandnani<br />Amelia Hussain</p>
+                        <p className='text-footer'>Pardis Habibi<br />Dhiren Hitescumar Daialal<br />Darien Perpepa</p>
+                        <p className='text-footer'>Darien Perpepa</p>
+                    </article>
+                </div>
             </div>
-            <div className='v-div'></div>
-            <div className='footer-content-item'>
-                <h2 className='text-footer-title'>Links</h2>
-                <article>
-                    <p className='text-footer'>Dashboard<br/>Issues<br/>EC</p>
-                    <p className='text-footer'>Services<br/>FAQ<br/>Account</p>
-                </article>
-            </div>
-            <div className='v-div'></div>
-            <div className='footer-content-item'>
-            <h2 className='text-footer-title'>Authors</h2>
-                <article>
-                    <p className='text-footer'>Roman Pretty<br/>Aarti Mukesh Chandnani<br/>Amelia Hussain</p>
-                    <p className='text-footer'>Pardis Habibi<br/>Dhiren Hitescumar Daialal<br/>Darien Perpepa</p>
-                    <p className='text-footer'>Darien Perpepa</p>
-                </article>
-            </div>
-        </div>
-      </footer>
-  );
+        </footer>
+    );
 };
 export default Footer;
