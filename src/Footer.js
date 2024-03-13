05ac6761b2f7b7db665d8bc90be43855da447ba9
ec22761 <ec22761@qmul.ac.uk>
2024-03-13 12:56:34 +0000

    Add underline and hover color to footer links

--- src/Footer.js
+++ src/Footer.js
@@ -14,8 +14,14 @@ const Footer = () => {
                 <div className='footer-content-item'>
                     <h2 className='text-footer-title'>Links</h2>
                     <article>
-                        <p className='text-footer'>Dashboard<br />Issues<br />EC</p>
-                        <p className='text-footer'>Services<br />FAQ<br />Account</p>
+                        <p className='text-footer'>
+                            <a href='#'>Dashboard<br /></a>
+                            <a href='#'>Issues<br /></a>
+                            <a href='#'>EC</a></p>
+                        <p className='text-footer'>
+                            <a href='#'>Services<br /></a>
+                            <a href='#'>FAQ<br /></a>
+                            <a href='#'>Account</a></p>
                     </article>
                 </div>
                 <div className='v-div'></div>
