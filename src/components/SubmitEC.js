7455c8148215741e39f1a56ee2adeff5574544f4
ameliah564 <ameliah3450@gmail.com>
2024-03-19 20:03:59 +0000

    Incomplete SubmitEC

--- src/components/SubmitEC.js
+++ src/components/SubmitEC.js
@@ -1,13 +1,6 @@
 import '../css/App.css';
 import React, { useState } from 'react';
 
-
-// INCOMPLETED
-
-
-
-
-
 const SubmitEC = () => {
   const [selectedOption, setSelectedOption] = useState('');
 
@@ -19,31 +12,95 @@ const SubmitEC = () => {
   return (
     <div id="submit-ec-card">
       <form>
-        <label>
-          <input
-            type="radio"
-            value="StandardEC"
-            checked={selectedOption === 'StandardEC'}
-            onChange={handleOptionChange}
-          />
-          Standard EC
-        </label>
+        <div id="standard-ec">
+            <input
+              id="standardec"
+              type="radio"
+              value="StandardEC"
+              checked={selectedOption === 'StandardEC'}
+              onChange={handleOptionChange}
+            />
+            <label class="label-text" for="standardec"> Standard EC </label>
+          </div>
         <br />
-        <label>
+        <div id="urgent-ec">
           <input
+            id="urgentec"
             type="radio"
             value="UrgentEC"
             checked={selectedOption === 'UrgentEC'}
             onChange={handleOptionChange}
           />
-          Urgent EC
-        </label>
+          <label class="label-text" for="urgentec"> Urgent EC </label>
+        </div>
+        <br />
+        <div id="reason-for-ec">
+          <label class="label-text"> Reason for EC </label>
+          <br />
+          <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+            <div id="myDropdown" class="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div id="summary">
+          <label for="summary" class="label-text" >Summary: </label>
+          <input 
+          type="text"
+          id="summary" 
+          name="summary-box"
+          />        
+        </div>
+        <br />
+        <div id="affected-assessments">
+          <label class="label-text"> Affected assessments </label>
+            <div class="dropdown">
+              <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+              <div id="myDropdown" class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div> 
+          </div>
+        <br/>
+        <div id="attach-file">
+          <label for="file-attachment" class="label-text">Supporting Evidence: </label>
+          <input 
+            type="file" 
+            id="file-attachment" 
+            name="filename" 
+          />   
+        </div>
+        <br /> 
+        <input type="submit" />
       </form>
-      <div>
-        {/* <p>Selected option: {selectedOption}</p> */}
-      </div>
     </div>
   );
 };
 
 export default SubmitEC;
+
+
+/* show or hide dropdown options for reason for ec */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
